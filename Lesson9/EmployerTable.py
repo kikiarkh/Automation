373829a2b10ccd41dd42e46bed528bf017a7cc2c
kikiarkh <kat_rina13@mail.ru>
2024-08-18 22:02:50 +0300

    HomeWork9

--- Lesson9/EmployerTable.py
+++ Lesson9/EmployerTable.py
@@ -1,46 +1,73 @@
-from sqlalchemy import create_engine 
-from sqlalchemy.sql import text 
+from sqlalchemy import create_engine, text
 
 
 class EmployerTable:
 
     __scripts =  {
-        "select": "select * from company",
-        "max_id": "select MAX(id) from company",
-        "insert_company": text("insert into company(\"name\", \"description\") values (:new_name, :descr)"),
+        "select": text("select * from company"),
+        "max_id": text("select MAX(id) from company"),
+        "insert_company": text("insert into company(name, description) values (:company_name, :description)"),
         "insert_employer": text("insert into employee(company_id, first_name, last_name, phone) values (:id, :name, :surname, :phone)"),
         "list_employer": text("select * from employee where company_id = :select_id"),
         "delite_by_id": text("delete from company where id = :id_to_delete"),
-        "id_employer": "select MAX(id) from employee",
-        "update_employer": text("update employee set first_name = :new_name where id = :employer_id")
+        "id_employer": text("select MAX(id) from employee where company_id = :c_id"),
+        "update_employer": text("update employee set first_name = :new_name where id = :employer_id"),
+        "delite_employer": text("delete from employee where id = :id_delete")
         }
 
-    def __init__(self, connection_string):
-        self.db = create_engine(connection_string)
+    def __init__(self, engine):
+        self.db = create_engine(engine)
 
     def get_max_id(self):
-        self.db.execute(self.__scripts["max_id"]).fetchall()[0][0]
-
+        with self.db.connect() as connection:
+            result = connection.execute(self.__scripts["max_id"]).fetchall()[0][0]
+        return result
 
     def create_new_company(self, name: str, description: str):
-        self.db.execute(self.__scripts["insert_company"], new_name = name, descr = description).fetchall()
+        with self.db.connect() as connection:
+            result = connection.execute(self.__scripts["insert_company"], parameters=dict(company_name=name, description = description))
+            connection.commit()
+        return result
 
     def create_new_employee(self, company_id, first_name, last_name, phone):
-        self.db.execute(self.__scripts["insert_employer"], id = company_id, name = first_name, surname = last_name, phone = phone).fetchall()
+        with self.db.connect() as connection:
+            result = connection.execute(self.__scripts["insert_employer"], parameters=dict(id = company_id, name = first_name, surname = last_name, phone = phone))
+            connection.commit()
+        return result
 
     def get_list(self, company_id):
-        self.db.execute(self.__scripts["list_employer"], select_id = company_id).fetchall()
+        with self.db.connect() as connection:
+            result = connection.execute(self.__scripts["list_employer"], parameters=dict(select_id = company_id))
+        return result
 
     def delite_company(self, id):
-        self.db.execute(self.__scripts["delite_by_id"], id_to_delete = id).fetchall()
+        with self.db.connect() as connection:
+            connection.execute(self.__scripts["delite_by_id"], parameters=dict(id_to_delete = id))
+            connection.commit()
 
-    def get_id_employer(self):
-        self.db.execute(self.__scripts["id_employer"]).fetchall()[0][0]
+    def get_id_employer(self, company_id):
+        with self.db.connect() as connection:
+            result = connection.execute(self.__scripts["id_employer"], parameters=dict(c_id=company_id)).fetchall()[0][0]
+        return result
 
     def get_info(self, id):
-        self.db.execute(self.__scripts["list_employer"], select_id = id).fetchall()
+        with self.db.connect() as connection:
+            result=  connection.execute(self.__scripts["list_employer"], parameters=dict(select_id = id))
+            connection.commit()
+        return result
 
     def change_info(self, new_name, id):
-        self.db.execute(self.__scripts["update_employer"],new_name = new_name, employer_id = id).fetchall()
+        with self.db.connect() as connection:
+            result = connection.execute(self.__scripts["update_employer"], parameters=dict(new_name = new_name, employer_id = id))
+            connection.commit()
+        return result
+    
+    def delite_employer(self, id):
+        with self.db.connect() as connection:
+            connection.execute(self.__scripts["delite_employer"],parameters=dict(id_delete = id))
+            connection.commit()
+
+           
+
 
 
--- Lesson9/test_employee.py
+++ Lesson9/test_employee.py
@@ -1,31 +1,23 @@
 import pytest
+import json
 from Employee import Employer
 from EmployerTable import EmployerTable
 
 api = Employer("https://x-clients-be.onrender.com")
-db = EmployerTable("postgresql://x_clients_user:95PM5lQE0NfzJWDQmLjbZ45ewrz1fLYa@dpg-cqsr9ulumphs73c2q40g-a.frankfurt-postgres.render.com/x_clients_db_fxd0")
+db = EmployerTable("postgresql+psycopg2://x_clients_user:95PM5lQE0NfzJWDQmLjbZ45ewrz1fLYa@dpg-cqsr9ulumphs73c2q40g-a.frankfurt-postgres.render.com/x_clients_db_fxd0")
 
 def test_create_new_employer():
-
-    new_name = "Cats and Dogs"
-    descr = "описание"
-    db.create_new_company(new_name, descr)
+    db.create_new_company("Cat and Dog", "description")
     max_id = db.get_max_id() 
-
-    company_id = max_id
-    first_name = "Hello"
-    last_name = "Kitty"
-    phone = "77777777777"
-    db_new_employer = db.create_new_employee(company_id, first_name, last_name, phone)
-
-    new_employer_id = db_new_employer['id']
-    api_new_employer = api.get_list(company_id)[0]
+    db_new_employer = db.create_new_employee(max_id, "Hello", "Kitty", "7777777777")
+    new_employer_id = db.get_id_employer(max_id)
+    api_employer_id = (api.get_info(new_employer_id))
 
     assert new_employer_id is not None
-    assert db_new_employer == api_new_employer
+    assert api_employer_id["firstName"] == "Hello"
     
-
-    db.delite_company(company_id)
+    db.delite_employer(new_employer_id)
+    db.delite_company(max_id)
 
 def test_get_info():
 
@@ -33,22 +25,19 @@ def test_get_info():
     descr = "string"
     db.create_new_company(name, descr)
     max_id = db.get_max_id() 
-
-    company_id = max_id
     first_name = "Tom"
     last_name = "Kitty"
     phone = "79997778899"
-    new_employer = db.create_new_employee(company_id, first_name, last_name, phone)
-
-    new_id = db.get_id_employer()
-
+    new_employer = db.create_new_employee(max_id, first_name, last_name, phone)
+    new_id = db.get_id_employer(max_id)
     db_get_info = db.get_info(new_id)
     api_info_employer = api.get_info(new_id)
 
-    assert new_id == db_get_info['id']
-    assert api_info_employer == db_get_info
+    assert api_info_employer["firstName"] == "Tom"
+    assert api_info_employer["lastName"] == "Kitty"
 
-    db.delite_company(company_id)
+    db.delite_employer(new_id)
+    db.delite_company(max_id)
 
 def test_change_employer_info():
 
@@ -56,22 +45,19 @@ def test_change_employer_info():
     descr = "string"
     db.create_new_company(name, descr)
     max_id = db.get_max_id() 
-
-    company_id = max_id
     first_name = "Tom"
     last_name = "Kitty"
     phone = "79997778899"
-    new_employer = db.create_new_employee(company_id, first_name, last_name, phone)
-
-    new_id = db.get_id_employer()
+    new_employer = db.create_new_employee(max_id, first_name, last_name, phone)
+    new_id = db.get_id_employer(max_id)
     new_name = "Jerry"
-
     db_change_info = db.change_info(new_name, new_id)
     api_change_info = api.get_info(new_id)
 
-    assert db_change_info == api_change_info
+    assert api_change_info["firstName"] == "Jerry"
 
-    db.delite_company(company_id)
+    db.delite_employer(new_id)
+    db.delite_company(max_id)
 
 def test_get_list_employer():
 
@@ -80,23 +66,27 @@ def test_get_list_employer():
     db.create_new_company(name, descr)
     max_id = db.get_max_id() 
 
-    company_id = max_id
     first_name = "Employer 1"
     last_name = "Petrov"
     phone = "79809876677"
-    db.create_new_employee(company_id, first_name, last_name, phone)
+    db.create_new_employee(max_id, first_name, last_name, phone)
+    new_id_1 = db.get_id_employer(max_id)
 
     first_name = "Employer 2"
     last_name = "Ivanov"
     phone = "79999999999"
-    db.create_new_employee(company_id, first_name, last_name, phone)
+    db.create_new_employee(max_id, first_name, last_name, phone)
+    new_id_2 = db.get_id_employer(max_id)
 
-    db_list_employer = db.get_list(company_id)
+    db_list_employer = db.get_list(max_id).fetchall()
+    api_list_employer = api.get_list(max_id)
 
-    api_list_employer = api.get_list(company_id)
-    assert len(db_list_employer) == 2
-    assert db_list_employer == api_list_employer
+    assert len(api_list_employer) == 2
+    assert len(db_list_employer) == len(api_list_employer)
+    assert api_list_employer
 
-    db.delite_company(company_id)
+    db.delite_employer(new_id_1)
+    db.delite_employer(new_id_2)
+    db.delite_company(max_id)
 
-    #pytest test_employee.py -v
+    # pytest test_employee.py -v
